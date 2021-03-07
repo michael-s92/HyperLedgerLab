b878bd53cca089d0801e9664c88e59d297747a64
milan <m.stefanovic@tum.de>
2021-03-07 20:39:09 +0100

    init articles

--- inventory/blockchain/src/contract/eureka/lib/generator.js
+++ inventory/blockchain/src/contract/eureka/lib/generator.js
@@ -34,6 +34,37 @@ function generateUser(type, index){
     };
 }
 
+function takeRandomFromList(list, number, rootElement){
+    let take = Utils.getRandomInt(number);
+
+    let returnList = [...list];
+    const ind = returnList.indexOf(rootElement);
+    if(ind > -1){
+        returnList.splice(ind, 1);
+    }
+
+    return Utils.getRandomSubarray(returnList, take);
+}
+
+function generateArticle(index, flag){
+
+    let title = flag + index + " " + Utils.generateRandomWord(10);
+    let author = authors[Utils.getRandomInt(authors.length)];
+    let coauthor_ids = takeRandomFromList(authors, parameters.max_coauthors, author);
+    let refauthor_ids = takeRandomFromList(authors, parameters.max_ref_authors, author);
+    let fee = Utils.getRandomInt(1000);
+    let lref = Utils.generateRandomString(512);
+
+    return {
+        title: title,
+        author: author,
+        coauthor_ids: coauthor_ids,
+        refauthor_ids: refauthor_ids,
+        fee: fee,
+        lref: lref
+    }
+}
+
 for (let i = 0; i < parameters.authors; i++){
     authors.push(generateUser(authorUserType, i));
 }
@@ -46,10 +77,15 @@ for (let i = 0; i < parameters.editors; i++){
     editors.push(generateUser(editorUserType, i));
 }
 
+for (let i = 0; i < parameters.init_articles; i++){
+    initArticle.push(generateArticle(i, "I"));
+}
+
 const json = JSON.stringify({
     authors: authors,
     reviewers: reviewers,
-    editors: editors
+    editors: editors,
+    initArticle: initArticle
 }, null, 4);
 
 fs.writeFile('seeds.json', json, function(err) {
