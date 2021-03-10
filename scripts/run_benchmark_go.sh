e40e6712bb62fac7e7f31b69bf305ce4983bddf7
milan <m.stefanovic@tum.de>
2021-03-11 00:06:47 +0100

    generator run

--- scripts/run_benchmark_go.sh
+++ scripts/run_benchmark_go.sh
@@ -29,7 +29,7 @@ fi
 benchmark_dir=inventory/blockchain/benchmark/$chaincode
 cd $benchmark_dir && npm install # && node generator.js
 set -x
-cd $contract_dir/ && ./hack/generator
+cd $contract_dir && $contract_dir/hack/generator
 #cp $benchmark_dir/seeds.json $contract_dir/
 sleep 2s
 set +x
